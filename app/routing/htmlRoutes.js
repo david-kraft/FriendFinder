b582e7c77e52075389e3e486dca5648896a7be4a
David Kraft <kraft153@umn.edu>
2018-06-12 00:32:32 -0500

    Good start on front-end

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,12 +6,12 @@ const path = require("path");
 module.exports = function(app){
     // HTML Get requests
 
-    // Send client to /public/survey.html if cliet gets /survey
+    // Send client to /public/survey.html if client gets /survey
     app.get("/survey", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 
-    // Send client to /public/home.html if cliet gets anything else
+    // Send client to /public/home.html if client gets anything else
     app.get("*", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
